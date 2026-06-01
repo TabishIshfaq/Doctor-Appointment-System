60214bb8d3b2dafddaaa9be568c8634d059b43c5
TabishIshfaq <124789184+TabishIshfaq@users.noreply.github.com>
2026-06-01 15:31:45 +0500

    Fix API URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
 // export const BASE_URL='http://localhost:5000/api/v1';
 // export const token = localStorage.getItem("token");
-export const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:5000/api/v1';
+export const BASE_URL =
+  'https://doctor-appointment-system-production-b097.up.railway.app/api/v1';
 export const token = localStorage.getItem("token");
