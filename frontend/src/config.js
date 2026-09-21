faff207e51eeb64d1103938ae293de5af0a0e61b
TabishIshfaq <124789184+TabishIshfaq@users.noreply.github.com>
2026-09-21 12:21:08 +0500

    Update backend API URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // export const BASE_URL='http://localhost:5000/api/v1';
 // export const token = localStorage.getItem("token");
 export const BASE_URL =
-  'https://doctor-appointment-system-production-b097.up.railway.app/api/v1';
+  'https://doctor-appointment-system-fwqq.onrender.com/api/v1';
 export const token = localStorage.getItem("token");
